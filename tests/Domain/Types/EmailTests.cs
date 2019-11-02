df103e3384b28a2449ec6e85c00ab194dd2afe66
Naker90 <Antoniojesussg96@gmail.com>
2019-11-02 19:55:17 +0100

    green ShouldReturnsErrorWhenEmailFormatIsInvalid

---
+++ tests/Domain/Types/EmailTests.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SaltedPasswordHashing.Src.Domain.Types;
+
+namespace SaltedPasswordHashing.Test.Domain.Types
+{
+    [TestClass]
+    public class EmailTests
+    {
+        [TestMethod]
+        public void ShouldCreatesEmail()
+        {
+            var userEmail = "user@email.com";
+
+            ValidationResult<Email> result = Email.Create(value: userEmail);
+
+            Assert.IsTrue(result.IsValid);
+            Assert.AreEqual(result.Result.Value, userEmail);
+        }
+
+        [TestMethod]
+        public void ShouldReturnsErrorWhenEmailFormatIsInvalid()
+        {
+            var userEmail = "invalid.com";
+
+            ValidationResult<Email> result = Email.Create(value: userEmail);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(result.Error, Error.InvalidFormat);
+        }
+    }
+}
