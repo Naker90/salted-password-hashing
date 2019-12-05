6e9ea9f6e82c93bae405fc96577e490f34079f4c
Naker90 <Antoniojesussg96@gmail.com>
2019-12-06 00:32:34 +0100

    #refactor extract methods and fields

--- tests/Domain/User/SignUp/UserSignUpCommandTests.cs
+++ tests/Domain/User/SignUp/UserSignUpCommandTests.cs
@@ -10,18 +10,24 @@ namespace SaltedPasswordHashing.Test.Domain.User.SignUp
     [TestClass]
     public class UserSignUpCommandTests
     {
+        private Mock<UserRepository> userRepository;
+        private Mock<IUserIdCreator> userIdCreator;
+        private UserSignUpCommand command;
+
+        [TestInitialize]
+        public void Init()
+        {
+            userRepository = new Mock<UserRepository>();
+            userIdCreator = new Mock<IUserIdCreator>();
+            command = new UserSignUpCommand(
+                userRepository: userRepository.Object,
+                userIdCreator: userIdCreator.Object);
+        }
+
         [TestMethod]
         public void ShouldSignUpUser()
         {
-            var userRepository = new Mock<UserRepository>();
-            var userIdCreator = new Mock<IUserIdCreator>();
-            var command = new UserSignUpCommand(
-                userRepository: userRepository.Object,
-                userIdCreator: userIdCreator.Object);
-            UserSignUpRequest request = UserSignUpRequest.Create(
-                email: "user@email.com",                
-                password: "Passw0rd$"
-            ).Result; 
+            UserSignUpRequest request = CreateRequest();
             var userId = new Guid("F9168C5E-CEB2-4faa-B6BF-329BF39FA1E4");
             userIdCreator
                 .Setup(x => x.Create())
@@ -31,13 +37,28 @@ namespace SaltedPasswordHashing.Test.Domain.User.SignUp
                     x.Id.Value == userId
                     && x.Email.Value == request.Email.Value
                     && x.Password.Value == request.Password.Value)))
-                .Returns(new SaltedPasswordHashing.Src.Domain.User.User(
-                    new UserId(userId), request.Email, request.Password));
-
+                .Returns(BuildUser());
 
             var result = command.Execute(request);
 
             Assert.IsTrue(result.IsValid);
+        }
+
+        private UserSignUpRequest CreateRequest(){
+            return UserSignUpRequest.Create(
+                email: "user@email.com",                
+                password: "Passw0rd$"
+            ).Result;
+        }
+
+        private SaltedPasswordHashing.Src.Domain.User.User BuildUser(){
+            var userId = new Guid("F9168C5E-CEB2-4faa-B6BF-329BF39FA1E4");
+            var email = Email.Create("user@email.com").Result;
+            var password = Password.Create("Pass0word$").Result;
+            return new SaltedPasswordHashing.Src.Domain.User.User(
+                    id: new UserId(userId), 
+                    email: email, 
+                    password: password);
         }
     }
 }
