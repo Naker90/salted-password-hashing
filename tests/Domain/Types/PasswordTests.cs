b670c7d4827c9b2e94ffa0f6fa0bcfb68239e63c
Naker90 <Antoniojesussg96@gmail.com>
2019-12-07 23:31:30 +0100

    created missing test

--- tests/Domain/Types/PasswordTests.cs
+++ tests/Domain/Types/PasswordTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SaltedPasswordHashing.Src.Domain.Types;
+using SaltedPasswordHashing.Src.Domain.Security;
+using Moq;
 
 namespace SaltedPasswordHashing.Test.Domain.Types
 {
@@ -71,6 +73,43 @@ namespace SaltedPasswordHashing.Test.Domain.Types
 
             Assert.IsFalse(result.IsValid);
             Assert.AreEqual(result.Error, Error.InvalidFormat);
+        }
+
+        [TestClass]
+        public class WhenPasswordIsCreatedSuccessfuly : PasswordTests
+        {
+
+            private Mock<PasswordEncryptionService> passwordEncryptionService;
+            private Mock<SecurePseudoRandomGenerator> securePseudoRandomGenerator;
+            private Password password;
+
+            [TestInitialize]
+            public void Init()
+            {
+                passwordEncryptionService = new Mock<PasswordEncryptionService>();
+                securePseudoRandomGenerator = new Mock<SecurePseudoRandomGenerator>();
+                password = Password.Create(value: "Passw0rd$").Result;
+            }
+
+            [TestMethod]
+            public void ShouldEncryptPasswordWhenRequested()
+            {
+                var passwordSalt = new Password.Salt(value: 4235346654);
+                securePseudoRandomGenerator
+                    .Setup(x => x.Generate())
+                    .Returns(passwordSalt);
+                var encryptedPasswordOutput = "$2y$asdasdVDFJVw4rtfAFVSDfjc34t";
+                passwordEncryptionService
+                    .Setup(x => x.Encrypt(password.Value, passwordSalt))
+                    .Returns(encryptedPasswordOutput);
+
+                password.Encrypt(
+                    passwordEncryptionService: passwordEncryptionService.Object,
+                    securePseudoRandomGenerator: securePseudoRandomGenerator.Object);
+
+                Assert.AreEqual(password.Value, encryptedPasswordOutput);
+                Assert.AreEqual(password.SaltProp.Value, passwordSalt.Value);
+            }
         }
     }
 }
