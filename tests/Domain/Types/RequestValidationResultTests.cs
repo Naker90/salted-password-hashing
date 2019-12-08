c38bfec4ccbad8dfdb91b9ad539e0e0ab6b1c72e
Naker90 <Antoniojesussg96@gmail.com>
2019-12-08 23:24:56 +0100

    green ShouldCreateInvalidRequestResult

--- tests/Domain/Types/RequestValidationResultTests.cs
+++ tests/Domain/Types/RequestValidationResultTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SaltedPasswordHashing.Src.Domain.Types;
+using System.Collections.Generic;
 
 namespace SaltedPasswordHashing.Test.Domain.Types
 {
@@ -19,6 +20,24 @@ namespace SaltedPasswordHashing.Test.Domain.Types
             Assert.IsTrue(result.IsValid);
             Assert.AreEqual(result.Result.Value, requestForTest.Value);
             Assert.AreEqual(result.Errors.Count, 0);
+        }
+
+        [TestMethod]
+        public void ShouldCreateInvalidRequestResult()
+        {
+            var errors = new List<ValidationError<ErrorForTest>>
+            {
+                new ValidationError<ErrorForTest>(fieldId: "FieldId", error: ErrorForTest.AnyError)
+            }.AsReadOnly();
+            
+            var result = RequestValidationResult<RequestForTest, ErrorForTest>.CreateInvalidResult(
+                errors: errors
+            ); 
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(result.Errors[0].FieldId, errors[0].FieldId);
+            Assert.AreEqual(result.Errors[0].Error, errors[0].Error);
+            Assert.AreEqual(result.Errors.Count, 1);
         }
 
         private class RequestForTest
