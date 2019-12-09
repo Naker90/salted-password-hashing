085cfab86283f99db96d05537cda7bbb4f130898
Naker90 <Antoniojesussg96@gmail.com>
2019-12-09 16:11:30 +0100

    implement signup in console app

--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SaltedPasswordHashing.Src.Domain.User.SignUp;
+using SaltedPasswordHashing.Src.Repositories;
+using SaltedPasswordHashing.Src.Security;
+using System;
 
 namespace ConsoleApplication
 {
@@ -7,7 +10,56 @@ namespace ConsoleApplication
         static void Main(string[] args)
         {
             Console.WriteLine("[Salted password hashing console app]");
-            Console.WriteLine("[Menu] /n press 1 to signup /n press 2 to login");
+            while (true)
+            {
+                Console.WriteLine($"[Menu] {System.Environment.NewLine}" +  
+                    $"- press 1 to signup {System.Environment.NewLine}" +
+                    $"- press 2 to login {System.Environment.NewLine}" +
+                    "- press 3 to exit");
+                string line = Console.ReadLine();
+                if(line == "1")
+                {
+                    SignUp();
+                }
+                else if(line == "2")
+                {
+                    Console.Write("Login");
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        static void SignUp()
+        {
+            Console.WriteLine("Email: ");
+            var email = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            var password = Console.ReadLine();
+            var requestResult = UserSignUpRequest.Create(
+                email: email,
+                password: password
+            );
+            if(!requestResult.IsValid)
+            {
+                Console.WriteLine(requestResult.Errors);
+            }else{
+                var request = requestResult.Result;
+                var command = new UserSignUpCommand(
+                    userRepository: new CsvUserRepository(),
+                    passwordEncryptionService: new BCryptPasswordEncryptionService(),
+                    securePseudoRandomGenerator: new RNGSecurePseudoRandomGenerator()
+                );
+                var commandResult = command.Execute(request);
+                if(!commandResult.IsValid)
+                {
+                    Console.WriteLine(requestResult.Errors);
+                }else{
+                    Console.WriteLine("User registered successfuly!");
+                }
+            }
         }
     }
 }
